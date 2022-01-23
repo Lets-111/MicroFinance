754fe568ceffe51e272ba67ae381be3294843e97
Let s do it <dipanshu.magoo96@gmail.com>
2022-01-23 12:10:15 +0530

    added method getFinanceById

--- src/main/java/com/magoo/project/controller/MicroController.java
+++ src/main/java/com/magoo/project/controller/MicroController.java
@@ -1,5 +1,15 @@
 package com.magoo.project.controller;
 
 public class MicroController {
+  
+  
+  
+  
+  
+  @GetMapping("/getFinanceId")
+  public void getFinanceById()
+  {
+    System.out.printn("Hello finance by Id");
+  }
 
 }
